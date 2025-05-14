6948e2737947159982564977345b13106d48a64a
c-aguillaume <175580634+c-aguillaume@users.noreply.github.com>
2025-05-14 03:02:34 -0400

    installed cors and implemented it as middleware in my server.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors')
 
 
 const memberRouter = require('./routes/memberRoutes')
 
 
 //1) MIDDLEWARES 
+app.use(
+    cors())
 app.use(morgan('dev'))
 app.use(express.json());
 
